7cf5d742063d1cae8d28f78afc13e0cbba429ad3
Nicholas Lai <nevereverv@gmail.com>
2019-11-06 22:21:32 +0800

    prototype for 2 order list

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 /// 引用linebot SDK
 var linebot = require('linebot');
 
-let MAX_ROOM = 2;
+let MAX_ROOM = process.env.MAX_ROOM || 2;
+
 class _lists {
   constructor(roomId) {
     this.lists = new Map();
@@ -14,11 +15,16 @@ class _lists {
       console.log(key + ', ' + value);
     }
   }
+
+  getList() {
+    return this.lists;
+  }
 }
 
 class _allLists {
   constructor(){
     this.allLists = new Map();
+    return this;
     console.log("_allLists constructor!");
   }
 
@@ -31,8 +37,12 @@ class _allLists {
       this.allLists.set(roomId, new _lists(roomId));
       if(typeof(this.allLists.get(roomId) === "objects")) {
         return this.allLists.get(roomId);
+      } else {
+        return null;
       }
-    }
+    } else {
+      return null;
+    };
   }
 
   ls() {
@@ -40,7 +50,9 @@ class _allLists {
       console.log(key + ', ' + value);
     }
   }
-
+  getListSize() {
+    return this.allLists.size;
+  }
   getRoom(roomId) {
     return this.allLists.get(roomId);
   }
@@ -50,7 +62,7 @@ class _allLists {
 }
 
 console.log("start new _allLists()");
-var alllines = new _allLists();
+let alllines = new _allLists();
 console.log("end new _allLists()");
 
 // 用於辨識Line Channel的資訊
@@ -79,7 +91,12 @@ var checkRe = new RegExp(orderWhat
 bot
 .on('join', function (event) {
   console.log("join");
-  alllines.newRoom(event.source.roomId);
+  if(alllines.newRoom(event.source.roomId) === null) {
+    event.reply("Loading is full.");
+    bot.leaveRoom(event.source.roomId);
+  } else {
+    event.reply(`Loading: ${alllines.getListSize()}`);
+  }
 })
 .on('leave', function (event) {
   console.log("leave");
@@ -94,27 +111,37 @@ bot
   console.log(event.message.text);
   if(_cmd === 'join') {
     console.log("cmd join");
-    alllines.add(event.source.roomId);
-  } else if(_cmd === 'leave'){
-    console.log("cmd leave");
+    if(alllines.newRoom(event.source.roomId) === null) {
+      event.reply("Loading is full.");
+    } else {
+      event.reply(`Loading: ${alllines.getListSize()}`);
+    }
+  } else if(_cmd === 'leave') {
+    console.log(`cmd leave: ${event.source.roomId}`);
     alllines.delRoom(event.source.roomId);
+    bot.leaveRoom(event.source.roomId);
+  
+  } else if(_cmd === 'showRoomId'){
+    console.log(`roomId: ${event.source.roomId}`);
+
+  } else if(_cmd === 'showOnlineRoom'){
+    console.log(`rooms: ${alllines.getListSize()}`);
 
   } else if(_cmd === 'l'){
-    var replyMsg = "";
-    var _lists = alllines.getRoom(event.source.roomId).lists;
-
-    console.log(`count=${_lists.size}`);
-    for (var [key, value] of _lists) {
-      replyMsg += value + "\n"; 
+    try{
+      var replyMsg = "";
+      var _lists = alllines.getRoom(event.source.roomId).getList();
+  
+      console.log(`count=${_lists.size}`);
+      for (var [key, value] of _lists) {
+        replyMsg += value + "\n"; 
+      }
+      console.log(replyMsg);
+      event.reply(replyMsg);  
+    } catch(e) {
+      console.log(e);
     }
-    console.log(replyMsg);
 
-    event.reply(replyMsg).then(function (data) {
-
-    }).catch(function (error) {
-        // 當訊息回傳失敗後的處理
-        throw(error);
-    });
   } else if (_cmd === 'h' || _cmd === '?') {
     var replyMsg = "l: list \n h:cmd list";
     event.reply(replyMsg).then(function (data) {
@@ -129,19 +156,33 @@ bot
     var userName = "";
     var userOrder = event.message.text + "";
 
+    var _lists = alllines.getRoom(event.source.roomId).getList();
+    if(_lists === null) {
+      if(alllines.newRoom(event.source.roomId) === null) {
+        bot.reply("Loading is full.");
+      } else {
+        bot.reply(`Loading: ${alllines.getListSize()}`);
+      }
+    }
+
     //get user profile
     event.source.profile()
     .then(function (profile) {
       userName = profile.displayName;
     })
     .then(() => {
-      //parese data
-      var list = userOrder.split(' ');
-      console.log(event.source.userId + ": "+userName +" " + list);
-      
-      //add to list
-      _lists.set(event.source.userId, userName + ' ' + list[0] + ' ' + list[1] + ' ' + list[2]);
-    })
+      try {
+        //parese data
+        var list = userOrder.split(' ');
+        console.log(event.source.userId + ": "+userName +" " + list);
+
+        //add to list
+        _lists.set(event.source.userId, userName + ' ' + list[0] + ' ' + list[1] + ' ' + list[2]);
+      }
+      catch (e) {
+        console.log(e);
+      }
+    });
 
   } else {
     console.log("parse false");
@@ -160,5 +201,5 @@ bot
 
 // Bot所監聽的webhook路徑與port
 bot.listen('/linewebhook', process.env.PORT || 3000, function () {
-    console.log('[BOT已準備就緒]');
+  console.log('[BOT已準備就緒]');
 });
